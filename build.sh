2eca54e7197d58cb0fdfd4de9a993f2039bba9d4
Yuriy Lipchey <yuriy.lipchey.work@gmail.com>
2023-04-09 19:55:08 +0300

    update build sh

--- build.sh
+++ build.sh
@@ -3,14 +3,14 @@ current_date_time=$(date)
 
 git config --global --add safe.directory "${PWD}"
 git reset --hard
-git checkout -b master
+git checkout master
 git pull
 yarn install --immutable
 node docker-prebuild.js
 yarn rw build api
 yarn rw build web
 
-git checkout -b deploy
+git checkout deploy
 mv misc/.gitignore .gitignore
 
 git add .
