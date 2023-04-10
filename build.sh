cf2326416010e1c13277d383f0fb9a8971457a66
Yuriy Lipchey <yuriy.lipchey.work@gmail.com>
2023-04-10 10:34:36 +0300

    update build.sh

--- build.sh
+++ build.sh
@@ -21,6 +21,7 @@ git add .
 git commit -m "Deploy ${current_date_time}"
 echo "https://${GIT_TOKEN}@github.com/${GITHUB_USERNAME}/${GITHUB_REPO}.git"
 
+git remote remove origin
 git remote add origin "https://${GITHUB_USERNAME}:${GIT_TOKEN}@github.com/${GITHUB_USERNAME}/${GITHUB_REPO}.git"
-git push --force
+git push "https://${GITHUB_USERNAME}:${GIT_TOKEN}@github.com/${GITHUB_USERNAME}/${GITHUB_REPO}.git" --force
 
