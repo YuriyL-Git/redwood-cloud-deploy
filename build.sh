79bc0102b9dcf9eb6a7990c99babffd8e351be9f
Yuriy Lipchey <yuriy.lipchey.work@gmail.com>
2023-04-11 20:02:56 +0300

    update build.sh

--- build.sh
+++ build.sh
@@ -4,23 +4,24 @@ current_date_time=$(date)
 
 
 git config --global --add safe.directory "${PWD}"
-#git reset --hard
-#git checkout master
-#git fetch --all
-#git reset --hard origin/master
+git reset --hard
+git checkout master
+git fetch --all
+git reset --hard origin/master
 
 yarn install --immutable
 node docker-prebuild.js
 yarn rw build api
 yarn rw build web
 
+git checkout HEAD -- redwood.toml
 git checkout deploy
 
 git add .
 git commit -m "Deploy ${current_date_time}"
-echo "http://${GIT_TOKEN}@github.com/${GITHUB_USERNAME}/${GITHUB_REPO}.git"
+echo "https://${GIT_TOKEN}@github.com/${GITHUB_USERNAME}/${GITHUB_REPO}.git"
 
 git remote remove origin
-git remote add origin "http://${GIT_TOKEN}@github.com/${GITHUB_USERNAME}/${GITHUB_REPO}.git"
-git push "http://${GITHUB_USERNAME}:${GIT_TOKEN}@github.com/${GITHUB_USERNAME}/${GITHUB_REPO}.git" --force
+git remote add origin "https://${GITHUB_USERNAME}:${GIT_TOKEN}@github.com/${GITHUB_USERNAME}/${GITHUB_REPO}.git"
+git push "https://${GITHUB_USERNAME}:${GIT_TOKEN}@github.com/${GITHUB_USERNAME}/${GITHUB_REPO}.git" --force
 
