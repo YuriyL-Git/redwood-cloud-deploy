672ebfbe8d8f8ee2741f95775c876bbf17dacb23
Yuriy Lipchey <yuriy.lipchey.work@gmail.com>
2023-04-11 20:22:17 +0300

    update build

--- serve-api.sh
+++ serve-api.sh
@@ -3,5 +3,6 @@
 # NOTE: Requires binaries. Install with:
 # yarn global add @redwoodjs/api-server @redwoodjs/internal prisma
 
+prisma generate
 prisma migrate deploy --schema ./api/db/schema.prisma
 rw-server api
