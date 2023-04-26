19bcfdc4bc09bfd1ff821ab920749f5cd5b2bdb9
YuriyL-Git <YuriyL-Git@users.noreply.github.com>
2023-04-26 15:36:41 +0000

    build

--- web/dist/static/js/122.7283a4a8.chunk.js
+++
@@ -1,2 +0,0 @@
-/*! For license information please see 122.7283a4a8.chunk.js.LICENSE.txt */
-"use strict";(self.webpackChunkweb=self.webpackChunkweb||[]).push([[122],{122:function(e,t,s){s.r(t);var a=s(27378),n=s(31786);t.default=function(){return a.createElement(a.Fragment,null,a.createElement(n.MetaTags,{title:"About",description:"About page"}),a.createElement("p",null,"This site was created to demonstrate my mastery of Redwood: Look on my works, ye mighty, and despair!hosttest test ssl"))}}}]);
