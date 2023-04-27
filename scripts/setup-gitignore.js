b242a0f5fd25c6b1da535a44cef8eae033a24b26
Yuriy Lipchey <yuriy.lipchey.work@gmail.com>
2023-04-27 11:52:29 +0300

    add spaces

--- scripts/setup-gitignore.js
+++ scripts/setup-gitignore.js
@@ -6,6 +6,15 @@ const encoding = 'utf8';
 setTimeout(async () => {
   const pathToFile = path.join(__dirname, '..', '.gitignore');
   const packageContent = await fs.readFile(pathToFile, encoding);
-  const result = packageContent.replace(/dist\n/, '');
+  let result = packageContent.replace(/dist\n/, '');
+
+  if (!result.includes('ssl_key.key')) {
+    result = result + 'ssl_key.key \n';
+  }
+
+  if (!result.includes('ssl_certificate.crt')) {
+    result = result + 'ssl_certificate.crt \n';
+  }
+
   fs.writeFile(pathToFile, result, encoding);
 }, 0);
