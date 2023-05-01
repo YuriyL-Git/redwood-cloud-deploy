3c3e63b01bfd85dc42ea8fce69155b695dbd5781
Yuriy Lipchey <yuriy.lipchey.work@gmail.com>
2023-05-01 20:14:51 +0300

    test

--- scripts/restore-db.sh
+++ scripts/restore-db.sh
@@ -2,11 +2,11 @@
 #sudo nano restore-db.sh
 #sudo chmod +x restore-db.sh &&  sudo ./restore-db.sh
 path_to_volume=redwood-cloud-deploy_pgdata
-backup_name=backup_2023-04-28_16_55_32.zip
+backup_name=backup_2023-05-01_17_06_01.zip
 
 # shellcheck disable=SC2164
 cd /home/db-backups
 unzip ${backup_name}
 
 sudo rm -r /var/lib/docker/volumes/"${path_to_volume}"
-mv ${path_to_volume} /var/lib/docker/volumes
+mv /home/db-backups/var/lib/docker/volumes/${path_to_volume} /var/lib/docker/volumes
