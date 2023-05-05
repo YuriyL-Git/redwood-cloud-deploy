a673a94a4d63664938d4ffb636178a7c1e072254
Yuriy Lipchey <yuriy.lipchey.work@gmail.com>
2023-05-05 21:49:07 +0300

    test

--- scripts/backup-db.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#sudo chmod +x backup-db.sh &&  sudo ./backup-db.sh
-mkdir -p /home/db-backups
-
-cd /var/lib/docker/volumes
-sudo zip -r /home/db-backups/backup_$(date +"%Y-%m-%d_%H_%M_%S").zip  ./redwood-cloud-deploy_pgdata
